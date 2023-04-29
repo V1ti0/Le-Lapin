e0aab21e97d286698907a1166d45a4afa3f1a743
V1ti0 <2140218@cmontmorency.qc.ca>
2023-04-29 16:45:15 -0400

    comitting suicide

---
+++ web_projet_cool/script/script.js
@@ -0,0 +1,22 @@
+
+var tl = gsap.timeline({repeat: -1});
+tl.to(".scroll-icon", {opacity: 0, opacity: 1, duration: 0.5});
+tl.to(".scroll-icon", { y: 50, duration: 0.5});
+tl.to(".scroll-icon", {opacity: 0, duration: 0.5});
+tl.repeat();
+
+let delayscroll;
+
+window.addEventListener("scroll", (delayscroll)=>{
+
+    clearTimeout(delayscroll);
+  
+    body.classList.add("is-scrolling");
+  
+    delayscroll = setTimeout(function(){
+  
+      body.classList.remove("is-scrolling");
+  
+    }, 1000)
+  
+  }))
