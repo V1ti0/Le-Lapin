ac51fe2837cab7c2274ef29e5ee9dbf7bf0df181
V1ti0 <2140218@cmontmorency.qc.ca>
2023-05-01 18:33:10 -0400

    sprite

--- web_projet_cool/script/script.js
+++ web_projet_cool/script/script.js
@@ -72,3 +72,58 @@ chapterfourBOOK.fromTo(".book", {
     x:"0vw", duration: 5
 })
 
+let chapterfiveBUNNY = gsap.timeline();
+
+chapterfiveBUNNY.fromTo(".spritefive-bunny" , {
+    x:"-800%" 
+},{
+    x:"-70%", ease: "power1.out", duration: 3, 
+})
+.to(".spritefive-bunny" , {
+rotate: -10, duration: 0.25
+})
+
+
+chapterfiveBUNNY.fromTo(".spritefive-bunny", {
+    x:"-70%", scale: 1
+},{
+    x:"105%", y:"-31%", scale: 0.07, rotate:-15, delay: 0.75,duration: 2
+})
+
+
+
+let chapterfivePLANE = gsap.timeline();
+
+chapterfivePLANE.fromTo(".paper-plane", {
+    x:"0%" , y:"0%", scale: 1
+},{
+    x:"20%", y:"-25%", scale: 0.10, rotate:-15, duration: 2
+})
+
+var delayyPLANE = chapterfivePLANE.delay()
+
+chapterfivePLANE.delay(4)
+
+let chaptersixPLANE = gsap.timeline();
+
+chaptersixPLANE.fromTo(".paper-plane-two", {
+    x:"-100%" , y:"500", scaleX: 1, scale: 1,
+},{
+    x:"100%" , y:"-300" , scaleX: 1, scale: 1, duration:5
+})
+
+chaptersixPLANE.fromTo(".paper-plane-two", {
+    x:"100%" , y:"-300" , scaleX: -0.1, scale: 0.10,
+},{
+    x:"0%" , y:"-400" , duration:5
+})
+
+
+let chaptersixBUNNY = gsap.timeline();
+
+chaptersixBUNNY.fromTo(".spritesix-bunny", {
+    x:"-800%" , y:"600"
+},{
+    x:"0%" , y:"0" , duration:2
+})
+
